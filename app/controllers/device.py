569776c3fa2169534abbc007820fde0bea8f736c
Skrymer123 <vojtechtlamka@seznam.cz>
2021-01-24 15:03:18 +0100

    messages updated authentication for device creation required now

--- app/controllers/device.py
+++ app/controllers/device.py
@@ -14,42 +14,47 @@ from app.models import Device, Url
 
 @require_http_methods(['GET', 'POST'])
 def create(request):
-    if request.method == "GET":
-        choices = Device._meta.get_field('type').choices
-        return render(request, 'device/create.html', {'choices': choices})  # choices = array of possible device types
+    if request.user.is_authenticated:
+        if request.method == "GET":
+            choices = Device._meta.get_field('type').choices
+            return render(request, 'device/create.html', {'choices': choices})  # choices = array of possible device types
+
+        else:
+            device_type = request.POST.get('type')                              # device type
+            if not device_type:
+                device_type = 'UK'
+
+            new_device = Device.objects.create(                                 # device creation
+                type=device_type,
+                ip_address=request.POST.get('ip_address'),
+                name=request.POST.get('name'),
+                description=request.POST.get('description'),
+            )
+            new_device.save()
+
+            i = 1
+            while True:                                                         # urls creation
+                url_name = request.POST.get('url_' + str(i) + '_name')
+                url_url = request.POST.get('url_' + str(i) + '_url')
+                if not url_name or not url_url:
+                    break
+                new_url = Url.objects.create(
+                    name=url_name,
+                    url=url_url,
+                    device=new_device,
+                )
+                new_url.save()
+
+                if i is 1:                                                      # set default_url for the device
+                    Device.objects.filter(ip_address=new_device.ip_address).update(default_url=new_url)
+
+                i += 1
+
+            messages.success(request, 'Vytvořeno zařízení ' + new_device.name + ".")
+            return redirect('index')
 
     else:
-        device_type = request.POST.get('type')                              # device type
-        if not device_type:
-            device_type = 'UK'
-
-        new_device = Device.objects.create(                                 # device creation
-            type=device_type,
-            ip_address=request.POST.get('ip_address'),
-            name=request.POST.get('name'),
-            description=request.POST.get('description'),
-        )
-        new_device.save()
-
-        i = 1
-        while True:                                                         # urls creation
-            url_name = request.POST.get('url_' + str(i) + '_name')
-            url_url = request.POST.get('url_' + str(i) + '_url')
-            if not url_name or not url_url:
-                break
-            new_url = Url.objects.create(
-                name=url_name,
-                url=url_url,
-                device=new_device,
-            )
-            new_url.save()
-
-            if i is 1:                                                      # set default_url for the device
-                Device.objects.filter(ip_address=new_device.ip_address).update(default_url=new_url)
-
-            i += 1
-
-        messages.success(request, 'Created device ' + new_device.name)
+        messages.error(request, 'Přístup zablokován.')
         return redirect('index')
 
 
@@ -63,7 +68,7 @@ def edit(request, device_id):
             device['urls'] = device.urls.all()
         return render(request, 'device/edit.html', {'device': device})
     else:
-        messages.success(request, 'Edited device ' + str(device.name))
+        messages.success(request, 'Změněno zařízení ' + str(device.name) + ".")
         return redirect('index')
 
 
@@ -72,9 +77,9 @@ def delete(request, device_id):
     device = Device.objects.get(id=device_id)
     deleted = Device.objects.get(id=device_id).delete()
     if deleted:
-        messages.success(request, "Deleted device " + device.name)
+        messages.success(request, "Smazáno zařízení " + device.name + ".")
     else:
-        messages.error(request, "Device " + device.name + " couldn't be deleted.")
+        messages.error(request, "Nelze smazat zařízení " + device.name + ".")
     return redirect('index')
 
 
